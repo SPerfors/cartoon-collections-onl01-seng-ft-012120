8c2d3ec40b86c3941b004e1bf5521a91957d545a
Steven Perfors <stevecanbuild@gmail.com>
2020-01-26 23:46:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,12 +6,11 @@ def roll_call_dwarves(array)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls
 end
 
 def find_the_cheese# code an argument here
