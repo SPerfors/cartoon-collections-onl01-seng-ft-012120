82233982510fba7f69a3224fcf60651f9ab7ee67
Steven Perfors <stevecanbuild@gmail.com>
2020-01-26 23:54:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,8 @@ def long_planeteer_calls
     False
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+  i = 0 
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
